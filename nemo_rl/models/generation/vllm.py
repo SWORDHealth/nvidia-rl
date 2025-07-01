455b3377d17187b6f6dfe43d45142bc7603c45e7
Jimmy Zhang <jiemingz@nvidia.com>
2025-07-01 08:55:15 -0700

    remove most of the hardcoding
    Signed-off-by: Jimmy Zhang <jiemingz@nvidia.com>

--- nemo_rl/models/generation/vllm.py
+++ nemo_rl/models/generation/vllm.py
@@ -356,9 +356,8 @@ class VllmGenerationWorker:
 
             if self.cfg["vllm_cfg"].get("first_layer_layers_in_bf16", True):
                 print("Using FIRST_LAST_LAYERS_IN_BF16!")
-                fp8.FIRST_LAST_LAYERS_IN_BF16 = True
-                fp8_block_quant_cfg['ignored_layers'] = fp8.get_first_last_layer_param_names(
-                    self.model_name
+                fp8_block_quant_cfg['ignored_layers'] = fp8.get_params_in_first_last_n_layers(
+                    self.model_name, 1
                 )
 
             if self.cfg["vllm_cfg"].get("use_deep_gemm", False):
@@ -372,6 +371,7 @@ class VllmGenerationWorker:
 
 
         llm_kwargs = dict(
+            enforce_eager=True,
             model=self.model_name,
             load_format=load_format,
             # vllm==0.10.0 breaks skip_tokenizer_init=True.
