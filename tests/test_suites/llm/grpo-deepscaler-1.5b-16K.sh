1aa088b60bd8c50fb96d705d7f618156e6ca05c6
Terry Kong <terryk@nvidia.com>
2025-08-05 16:30:39 -0700

    chore: relocate deepscaler configs + add deepscaler CI tests
    Signed-off-by: Terry Kong <terryk@nvidia.com>

---
+++ tests/test_suites/llm/grpo-deepscaler-1.5b-16K.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+source $SCRIPT_DIR/common.env
+
+# ===== BEGIN CONFIG =====
+NUM_NODES=1
+STEPS_PER_RUN=$((530-240))
+MAX_STEPS=$((530-240))
+NUM_RUNS=$(( (MAX_STEPS + STEPS_PER_RUN - 1) / STEPS_PER_RUN ))  # Round up
+NUM_MINUTES=240
+# ===== END CONFIG =====
+
+exit_if_max_steps_reached
+
+# Use checkpoint created from the 8K checkpoint in grpo-deepscaler-1.5b-8K.sh
+if [[ -z "$CACHED_MODEL_PATH" ]]; then
+    echo "Need to set CACHED_MODEL_PATH to the path to the trained 8K checkpoint"
+    exit 1
+fi
+
+# Run the experiment
+cd $PROJECT_ROOT
+uv run examples/run_grpo_math.py \
+    --config $CONFIG_PATH \
+    policy.model_name=$CACHED_MODEL_PATH \
+    grpo.max_num_steps=$MAX_STEPS \
+    logger.log_dir=$LOG_DIR \
+    logger.wandb_enabled=True \
+    logger.wandb.project=nemo-rl \
+    logger.wandb.name=$EXP_NAME \
+    logger.monitor_gpus=True \
+    logger.tensorboard_enabled=True \
+    checkpointing.enabled=True \
+    checkpointing.checkpoint_dir=$CKPT_DIR \
+    $@ \
+    2>&1 | tee $RUN_LOG
+
+# Convert tensorboard logs to json
+uv run tests/json_dump_tb_logs.py $LOG_DIR --output_path $JSON_METRICS
+
+# Only run metrics if the target step is reached
+if [[ $(jq 'to_entries | .[] | select(.key == "train/loss") | .value | keys | map(tonumber) | max' $JSON_METRICS) -ge $MAX_STEPS ]]; then
+    uv run tests/check_metrics.py $JSON_METRICS \
+        'mean(data["train/token_mult_prob_error"]) < 1.1' \
+        'data["train/token_mult_prob_error"]["290"] < 1.1'
+fi
+
+# Convert 16k checkpoint
+uv run examples/converters/convert_dcp_to_hf.py \
+  --config=results/grpo/step_290/config.yaml \
+  --dcp-ckpt-path=results/grpo/step_290/policy/weights \
+  --hf-ckpt-path=results/grpo-deepscaler-16k-290-hf
+
+# Run eval
+uv run examples/run_eval.py \
+    generation.model_name=results/grpo-deepscaler-16k-290-hf \
+    data.prompt_file=examples/prompts/cot.txt \
+    generation.vllm_cfg.max_model_len=32768
