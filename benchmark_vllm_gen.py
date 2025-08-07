9a4f334fa730e23711e45e300520b28997de2d65
Brian Yu <bxyu@nvidia.com>
2025-08-07 15:54:22 -0700

    remove extraneous envvars

--- benchmark_vllm_gen.py
+++ benchmark_vllm_gen.py
@@ -9,13 +9,9 @@ from vllm import LLM, SamplingParams
 
 @ray.remote
 class VLLMWorker:
-    def __init__(self, llm_kwargs: dict, env_vars: dict, sampling_params: dict):
+    def __init__(self, llm_kwargs: dict, sampling_params: dict):
         self.sampling_params = SamplingParams(**sampling_params)
 
-        print("Initial CUDA_VISIBLE_DEVICES", os.environ.get("CUDA_VISIBLE_DEVICES"))
-        for k, v in env_vars.items():
-            os.environ[k] = v
-        print("Final CUDA_VISIBLE_DEVICES", os.environ.get("CUDA_VISIBLE_DEVICES"))
         self.llm = LLM(**llm_kwargs)
 
     def generate(self, prompt: str):
@@ -27,7 +23,6 @@ if __name__ == "__main__":
     with open("vllm_args.json") as f:
         vllm_args = json.load(f)
     llm_kwargs = vllm_args["vllm_llm_kwargs"]
-    env_vars = vllm_args["environment_variables"]
     sampling_params = vllm_args["sampling_params"]
 
     NUM_GPUS = 8
@@ -35,7 +30,7 @@ if __name__ == "__main__":
 
     workers = []
     for _ in range(NUM_GPUS // TENSOR_PARALLEL_SIZE):
-        worker = VLLMWorker.options(num_gpus=TENSOR_PARALLEL_SIZE).remote(llm_kwargs, env_vars, sampling_params)
+        worker = VLLMWorker.options(num_gpus=TENSOR_PARALLEL_SIZE).remote(llm_kwargs, sampling_params)
         workers.append(worker)
 
     prompt = "Explain Ray in one sentence."
