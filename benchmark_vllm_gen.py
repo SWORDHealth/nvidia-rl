43df5244a1e1a3cf7c79e641b599dbb2f73f6ef9
Brian Yu <bxyu@nvidia.com>
2025-08-07 15:30:56 -0700

    try impl benchmark vllm gen

--- benchmark_vllm_gen.py
+++ benchmark_vllm_gen.py
@@ -1,31 +1,45 @@
 import json
 
-from subprocess import Popen
+import os
 
-from pydantic import BaseModel
+import ray
 
-from vllm import LLM
-
-
-class VLLMInstanceMapping(BaseModel):
-    local_rank: int  # Which GPU to start on
-    tensor_parallel_size: int
+from vllm import LLM, SamplingParams
 
 
-def get_mapping(tensor_parallel_size: int):
-    return [
-        VLLMInstanceMapping(local_rank=local_rank, tensor_parallel_size=tensor_parallel_size)
-        for local_rank in range(0, 8, tensor_parallel_size)
-    ]
+@ray.remote
+class VLLMWorker:
+    def __init__(self, llm_kwargs: dict, env_vars: dict, sampling_params: dict):
+        self.sampling_params = SamplingParams(**self.sampling_params)
 
+        print("Initial CUDA_VISIBLE_DEVICES", os.environ.get("CUDA_VISIBLE_DEVICES"))
+        for k, v in env_vars.items():
+            os.environ[k] = v
+        print("Final CUDA_VISIBLE_DEVICES", os.environ.get("CUDA_VISIBLE_DEVICES"))
+        self.llm = LLM(**llm_kwargs)
 
-NODE_MAPPING_8x1 = get_mapping(tensor_parallel_size=1)
-NODE_MAPPING_4x2 = get_mapping(tensor_parallel_size=2)
-NODE_MAPPING_2x4 = get_mapping(tensor_parallel_size=4)
-NODE_MAPPING_1x8 = get_mapping(tensor_parallel_size=8)
+    def generate(self, prompt: str):
+        outputs = self.llm.generate(prompt, self.sampling_params)
+        return outputs[0].outputs[0].text
 
 
 if __name__ == "__main__":
     with open("vllm_args.json") as f:
         vllm_args = json.load(f)
-    Popen()
+    llm_kwargs = vllm_args["vllm_llm_kwargs"]
+    env_vars = vllm_args["environment_variables"]
+
+    NUM_GPUS = 8
+    TENSOR_PARALLEL_SIZE = 1
+
+    workers = []
+    for _ in range(NUM_GPUS // TENSOR_PARALLEL_SIZE):
+        worker = VLLMWorker.options(num_gpus=TENSOR_PARALLEL_SIZE).remote(llm_kwargs, env_vars, dict())
+        workers.append(worker)
+
+    prompt = "Explain Ray in one sentence."
+    futures = [worker.generate.remote(prompt) for worker in workers]
+    responses = ray.get(futures)
+
+    for i, r in enumerate(responses):
+        print(f"[GPU {i}] {r}")
